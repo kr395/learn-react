d93217418531bef0f3f7788e00b704304b3840ff
Gopal Kumar Das <Krgopalkumar706@gmail.com>
2024-02-05 11:35:54 +0000

    Conditional Rendering

---
+++ src/UserGreeting.jsx
@@ -0,0 +1,26 @@
+// To learn Conditional Rendering
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function UserGreeting(props) {
+  // if(props.isLoggedIn){
+  //   return(
+  //     <div>
+  //       <h1>Welcome {props.username}</h1>
+  //       <p>You are logged in</p>
+  //     </div>
+  //   )
+  // }
+  return props.isLoggedIn ? (
+    <div>
+      <h1>Welcome {props.username}</h1>
+      <p>You are logged in</p>
+    </div>
+  ) : (
+    <h1>Please Login</h1>
+  );
+}
+UserGreeting.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  name: PropTypes.string,
+};
